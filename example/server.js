132d94ad649b18fd62a818b8fd9fc53c9daa41bc
Dmitri Voronianskyi <dmitri.voronianskyi@sumup.com>
2020-03-01 20:07:36 +0100

    update example

--- example/server.js
+++ example/server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const port = process.env.PORT || 6789;
 const app = express();
 
-const asyncResponse = (req, done) => {
+const delayedResponse = (req, done) => {
   const delay = Number(req.query.delay || 0);
 
   setTimeout(() => done(), delay);
@@ -15,19 +15,19 @@ const asyncResponse = (req, done) => {
 app.get('/favicon.ico', (req, res) => res.sendStatus(204));
 
 app.get('/script.js', (req, res) => {
-  asyncResponse(req, () => {
+  delayedResponse(req, () => {
     res.sendFile(path.resolve(__dirname, './script.js'));
   });
 });
 
 app.get('/image.png', (req, res) => {
-  asyncResponse(req, () => {
+  delayedResponse(req, () => {
     res.sendFile(path.resolve(__dirname, './image.png'));
   });
 });
 
 app.get('/', (req, res) => {
-  asyncResponse(req, () => {
+  delayedResponse(req, () => {
     res.setHeader('Server-Timing', 'redis;dur=53, proxy-api;dur=247.2');
     res.sendFile(path.resolve(__dirname, './index.html'));
   });

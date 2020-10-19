a0766d36e1501565f46fd4ce940cc424ccf92265
Dmitri Voronianskyi <dmitri.voronianskyi@sumup.com>
2020-10-19 13:15:30 +0200

    export es5 code as module too

--- rollup.config.js
+++ rollup.config.js
@@ -2,37 +2,56 @@ import { terser } from 'rollup-plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 
-export default {
-  input: 'lib/performance-observer.js',
+const baseConfig = {
+  input: 'dist/performance-observer.js'
+};
+
+const plugins = (opts = {}) => [
+  nodeResolve(),
+  babel({
+    babelHelpers: 'bundled',
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            browsers: ['ie 11']
+          }
+        }
+      ]
+    ]
+  }),
+  terser({
+    module: opts.terserModule,
+    mangle: true,
+    compress: true,
+    output: {
+      comments: false
+    }
+  })
+];
+
+const umdConfig = {
+  ...baseConfig,
   output: [
     {
-      file: 'dist/performance-observer.min.js',
+      file: 'dist/performance-observer.es5.umd.min.js',
       format: 'umd',
       name: 'performanceObserver'
     }
   ],
-  plugins: [
-    nodeResolve(),
-    babel({
-      babelHelpers: 'bundled',
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              browsers: ['ie 11']
-            }
-          }
-        ]
-      ]
-    }),
-    terser({
-      module: false,
-      mangle: true,
-      compress: true,
-      output: {
-        comments: false
-      }
-    })
-  ]
+  plugins: plugins({ terserModule: false })
 };
+
+const esmConfig = {
+  ...baseConfig,
+  output: [
+    {
+      file: 'dist/performance-observer.es5.min.js',
+      format: 'esm'
+    }
+  ],
+  plugins: plugins({ terserModule: true })
+};
+
+export default [umdConfig, esmConfig];
